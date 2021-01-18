4bf5903cb1259ead790aba97425e3067ea6ea8aa
Altaf Rhomen <67639404+altafrhomen@users.noreply.github.com>
2021-01-18 03:15:32 -0800

    Update install.py

--- linux|mac/install.py
+++ linux|mac/install.py
@@ -2,5 +2,5 @@ import os
 os.system("pip3 install proxyscrape")
 os.system("cp proxygrab.py proxygrab")
 os.system("chmod+x proxygrab")
-os.system("mv proxygrab /usr/bin/")
+os.system("mv proxygrab /usr/local/bin/")
 print("file copied")
